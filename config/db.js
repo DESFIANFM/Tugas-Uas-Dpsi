74de4cc84110fbcf585238309cda1f7ba9f4a58c
DESFIANFM <2200016077@webmail.uad.ac.id>
2024-07-25 17:45:26 +0700

    fix db config

--- config/db.js
+++ config/db.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-
-dotenv.config();
+const config = require('config');
+const db = config.get('mongoURI');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        });
         console.log('MongoDB Connected...');
     } catch (err) {
         console.error(err.message);
